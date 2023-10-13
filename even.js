323510e8e73183a2a5c6b407aa3d1d4a4279f634
Dhaval <makanidhaval9619@gmail.com>
2023-10-13 14:03:00 +0530

    Print first n element

--- even.js
+++ even.js
@@ -1,3 +1,5 @@
+//Print even numbers from array using array mathod
+
 const arr = [1,2,3,4,5,6,7,8,9,10];
 
 const fil = arr.filter(element => element % 2 ==0);
---
+++ first_n_element.js
@@ -0,0 +1,17 @@
+//Print fisrt n element
+
+const n = 10;
+
+for (let i = 1 ; i <= n ; i ++){
+    console.log(i);
+}
+
+
+/*
+User Input with CodeHS Library
+You should have noted that prompt returns the user input as a string, but what if you want the user input to return a number instead of a string? You can convert the string to an integer using parseInt()…or you could just use the CodeHS library! The CodeHS JavaScript library includes the following commands:
+
+readLine(prompt): Reads a string value from a user
+readInt(prompt): Reads an integer value from a user
+readFloat(prompt): Reads a float value from a user
+*/
